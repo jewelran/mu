5baa55afaaae8ad8d80bde56e009a91cd33c1b4c
jewelran <mdjewelrana6487@gmail.com>
2020-09-23 02:03:22 +0600

    news api data show

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import {Button } from '@material-ui/core';
 import './App.css';
+import News from './component/News/News';
+import Chart from './component/Chart/Chart';
+import Axios from 'axios';
 
 function App() {
+  const [articles, setArticle] = useState([]);
+  // useEffect( ()=> {
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1fd5ac840397475093caa20400d83bcb';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data => setArticle(data.articles))
+  // }, []);
+  useEffect(()=>{
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=1fd5ac840397475093caa20400d83bcb';
+    Axios(url)
+    .then(data=> setArticle(data.data.articles))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin :'50px'}}>
+      <Chart></Chart>
+      
+      {
+        articles.map(article => <News article = {article}></News>)
+      } <br/>
+     <Button variant="contained" color="primary">click me</Button>
+
     </div>
   );
 }
